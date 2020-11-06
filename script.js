d73713940dbd52cd4e25ca866473fd7d7c00c194
Riya Agrawal <drriyaagrawal1998@gmail.com>
2020-11-06 21:17:56 +0530

    change

--- script.js
+++ script.js
@@ -1,55 +1,24 @@
-<<<<<<< HEAD
-const File=require ('./models/file');
-const fs= require('fs');
-const connectDB=require('./config/db');
-connectDB();
-//deleteData
-async function fetchData(){
-  const pastDate=new Date(Date.now() - (1000*60*60*24));
-  //24 hours
-  const files=await File.find({ createdAt: {$lt: pastDate}});
-  if(files.length){
-    for(const file of files){
-      try{
-        fs.unlinkSync(file.path);
-        await file.remove();//Remove from database
-        console.log(`sucessfully deleted ${file.filename}`);
-      }
-      catch(err){
-        console.log(`Error while deleting file ${err}`);
-      }
+const connectDB = require('./config/db');
+const File = require('./models/file');
+const fs = require('fs');
 
+connectDB();
+
+// Get all records older than 24 hours
+async function fetchData() {
+    const files = await File.find({ createdAt : { $lt: new Date(Date.now() - 24 * 60 * 60 * 1000)} })
+    if(files.length) {
+        for (const file of files) {
+            try {
+                fs.unlinkSync(file.path);
+                await file.remove();
+                console.log(`successfully deleted ${file.filename}`);
+            } catch(err) {
+                console.log(`error while deleting file ${err} `);
+            }
+        }
     }
-      console.log('Job done!');
-  }
+    console.log('Job done!');
 }
 
 fetchData().then(process.exit);
-=======
-const File=require ('./models/file');
-const fs= require('fs');
-const connectDB=require('./config/db');
-connectDB();
-//deleteData
-async function fetchData(){
-  const pastDate=new Date(Date.now() - (1000*60*60*24));
-  //24 hours
-  const files=await File.find({ createdAt: {$lt: pastDate}});
-  if(files.length){
-    for(const file of files){
-      try{
-        fs.unlinkSync(file.path);
-        await file.remove();//Remove from database
-        console.log(`sucessfully deleted ${file.filename}`);
-      }
-      catch(err){
-        console.log(`Error while deleting file ${err}`);
-      }
-
-    }
-      console.log('Job done!');
-  }
-}
-
-fetchData().then(process.exit);
->>>>>>> d01f725311bdea659eae2327093acc39862bf049
